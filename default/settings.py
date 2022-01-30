b6443f61c253b934630c125fedcf87fe2c88f1a9
José Luis Anaya Calvete <isc.luis.an.c@hotmail.com>
2022-01-30 02:10:16 -0600

    Añadida dependencia de cors. Configuracion de cors añadida a django.

--- default/settings.py
+++ default/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = [os.environ['ALLOWED_HOST']]
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'control_gastos.apps.ControlGastosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -43,6 +44,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -128,3 +131,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
